f53deb68ecfcf669a1eb425614718967908aec11
kingjack05 <kingjack05@gmail.com>
2023-11-25 11:22:27 +0800

    add sitemap and robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config"
-
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
+    site: "https://personal-website-nine-gules.vercel.app/",
+    integrations: [react(), tailwind(), sitemap()],
 })
